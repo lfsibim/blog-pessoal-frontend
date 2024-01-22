16852c7d11263dc6fcd9236b9f5f16069bbd8e5a
Luis Sibim <141850109+lfsibim@users.noreply.github.com>
2024-01-22 14:00:23 -0300

    adicionado pagina temas, implementado cadastro e edicao de temas

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Login from './pages/login/Login';
 import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+
 
 function App() {
   return (
@@ -18,6 +21,10 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+
             </Routes>
           </div>
           <Footer />
