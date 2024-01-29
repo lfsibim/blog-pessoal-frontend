4cd4393ef6f9c883e94cc22a45fef818f28e66c8
Luis Sibim <141850109+lfsibim@users.noreply.github.com>
2024-01-29 15:03:17 -0300

    adicionado componente postagem e suas funcionalidades

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,9 @@
 import homeLogo from '../../assets/home.png'
 import './Home.css';
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagens/ModalPostagem';
+
+
 
 function Home() {
     return (
@@ -11,15 +15,18 @@ function Home() {
               <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
   
               <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
+              <ModalPostagem />
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
             </div>
+            </div>
+  
             <div className="flex justify-center ">
               <img src={homeLogo} alt="" className='w-2/3' />
+      
             </div>
           </div>
         </div>
+        <ListaPostagens />
       </>
     );
 }
